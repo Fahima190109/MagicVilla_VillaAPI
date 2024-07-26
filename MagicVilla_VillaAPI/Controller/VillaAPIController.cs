48d415e238a764efd2b2c0ab6267bb67eb9c5a2d
Fahima190109 <fahimaakhter4326@outlook.com>
2024-07-26 12:22:48 +0600

    First commit

---
+++ MagicVilla_VillaAPI/Controller/VillaAPIController.cs
@@ -0,0 +1,31 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controller
+{
+    //[Route("api/ Controller")]
+    [Route("api/ VillaAPIController")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            //return new List<VillaDTO>()
+            //{
+            //    new VillaDTO{Id=1,Name = "Mehek"},
+            //    new VillaDTO{Id=2,Name = "Fahima"}
+            //};
+            return VillaStore.villaList;
+        }
+      [HttpGet("id")]
+        //[tpGet("{id : int}")]
+        public VillaDTO Getvilla(int id)
+        {
+                return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_VillaAPI.Models.DTO;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public class VillaStore
+    {
+         public static  List<VillaDTO> villaList = new List<VillaDTO>
+            {
+                new VillaDTO{Id=1,Name = "Mehek"},
+                new VillaDTO{Id=2,Name = "Fahima"}
+            };
+    }
+}
